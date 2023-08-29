d641d3f57da4e59f60ec46d25752b417d96e0cf5
Anita Diamond <38940580+ddocs@users.noreply.github.com>
2023-08-29 14:16:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,13 +29,3 @@ function reset() {
 }
 
 
-// 1. Create a function, save(), which logs out the count when it's called
-// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    // 3. Render the variable in the saveEl using innerText
-    // NB: Make sure to not delete the existing content of the paragraph
-
-// function save() {
-//     let saved = count + " - "
-//     saveEl.innerText += saved
-   
-
